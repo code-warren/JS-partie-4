3c46455e9439c57c189a61cf9fdd0ae9179300eb
code-warren <warrenphilibert@gmail.com>
2022-02-04 20:26:38 -0300

    test

---
+++ cours/index.js
@@ -0,0 +1,28 @@
+//XMLHttpResquest
+
+function reqListener() {
+  //   console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+// req.open("get", "data.txt", true);
+// req.open("get", "data.json", true);
+req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+req.send();
+
+//------
+// FETCH
+//------
+
+// fetch("mon-url", "object d'obtions")
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => console.log(err));
+
+fetch("data.txt").then((res) => res.text());
+//   .then((data) => console.log(data));
+
+fetch("data.json").then((res) => res.json());
+// .then((data) => console.log(data));
